c130db612648a92c581b1d44354671bca81259af
zayedibnibrahim <zayed.ibn@gmail.com>
2021-05-02 23:19:03 +0600

    first blank push

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../Footer/Footer/Footer';
+import Header from '../Header/Header';
+import Hero from '../Hero/Hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Hero></Hero>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
